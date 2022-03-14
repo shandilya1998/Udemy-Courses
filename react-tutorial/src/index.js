bada12393c7692f59768e031ddbb4bfe52c1f0a9
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2022-03-14 23:34:39 +0530

    [REACT] commit section 3, video 19, JSX Rules

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom';
 
 // Component name must be caplitalised
 // Stateless Functional Component
+// A component must always return JSX
+// A component must return a single component
+// `div`, `section` or `React.Fragment` (<></>) must be the root parent of JSX tree
+// Follow HTML semantics
+// Use camelcase for html attributes
+// `class` attribute is replaced with `className`
+// Every elemt must be closed
+
 function Greeting(){
     return (
         <div>
@@ -12,6 +20,16 @@ function Greeting(){
     );
 }
 
+/*
+const Greeting = () => {
+    return React.createElement(
+        'h1',
+        {},
+        'Hello World!'
+    );
+};
+*/
+
 ReactDOM.render(
-    <Greeting/>, document.getElementById('root')
+    <Greeting />, document.getElementById('root')
 );
