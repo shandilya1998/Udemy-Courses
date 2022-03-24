bb31ff8779f6b6f7105c6540c27dd58f8fabe160
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2022-03-24 23:26:36 +0530

    [REACT] commit section 3, videos 25-27, Props, structuring, destructuring and children keyword in rect

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -15,22 +15,6 @@ import './index.css';
 // `class` attribute is replaced with `className`
 // Every elemt must be closed
 
-
-
-function Booklist(){
-    return (
-        <section className="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-        </section>
-    );
-}
-
 /*
 const Greeting = () => {
     return React.createElement(
@@ -41,33 +25,53 @@ const Greeting = () => {
 };
 */
 
-const Book = () => {
+// SETUP
+const src1 = 'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg';
+const src2 = 'https://images-eu.ssl-images-amazon.com/images/I/51S7KOWir7L._AC_UL604_SR604,400_.jpg';
+const title1 = 'The Psychology of Money';
+const title2 = 'Atomic Habits: The life-changing million copy bestseller';
+const author1 = 'Morgan Housel';
+const author2 = 'James Clear';
+
+const book3 = {
+    title: 'Ikigai: The Japanese secret to a long and happy life',
+    src: 'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL604_SR604,400_.jpg',
+    author: 'Héctor García'
+};
+
+const book4 = {
+    title: 'DO EPIC SHIT',
+    author: 'Ankur Warikoo',
+    src: 'https://images-eu.ssl-images-amazon.com/images/I/41+grDTP2FL._AC_UL604_SR604,400_.jpg'
+};
+
+function Booklist(){
+    return (
+        <section className="booklist">
+            <Book src={src1} title={title1} author={author1}/>
+            <Book src={src2} title={title2} author={author2}/>
+            <Book {...book3} />
+            <Book src={book4.src} title={book4.title} author={book4.author}/>
+        </section>
+    );
+}
+
+const Book = (props) => {
     return (
         <article className="book">
-            <Image />
-            <Title />
-            <Author />
+            <img src={props.src} alt=''></img>
+            <h1>{props.title}</h1>    
+            <h4 style={{
+                color: '#617d98',
+                fontSize: '0.75rem',
+                margin: '0.25rem'
+                }}>
+                    {props.author}
+            </h4>    
         </article>
     );
 };
 
-// Implicit Return
-const Image = () => <img 
-    src='https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg' alt=''></img>;
-
-// Explicit Return
-const Title = () => {
-    return (
-        <h1>The Psychology of Money</h1>
-    );
-};
-
-
-const Author = () => <h4 style={{
-    color: '#617d98',
-    fontSize: '0.75rem',
-    margin: '0.25rem'
-}}>Morgan Housel</h4>;
 
 ReactDOM.render(
     <Booklist />, document.getElementById('root')
